2b58e5f683a60ce02c4c0565894d7cdc0a899562
Andrew Makersone <andrewmakersone@gmail.com>
2020-03-11 23:43:34 +0300

    initial

--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,87 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: {
+      layout: 'main'
+    },
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/login',
+    name: 'Login',
+    meta: {
+      layout: 'empty'
+    },
+    component: () => import('../views/Login')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    meta: {
+      layout: 'empty'
+    },
+    component: () => import('../views/Register')
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Categories')
+  },
+  {
+    path: '/detail-record',
+    name: 'DetailRecord',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/DetailRecord')
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/History')
+  },
+  {
+    path: '/planning',
+    name: 'Planning',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Planning')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Profile')
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Record')
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
